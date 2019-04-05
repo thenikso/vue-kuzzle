fb22871bb7b3ef5e35a42f70c2e233d43d440640
Nicola Peduzzi <thenikso@gmail.com>
2019-04-05 13:32:19 +0200

    Fix options type export

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,5 +2,6 @@ import './vue';
 import { VueKuzzle } from './vue-kuzzle';
 import { KuzzleProvider } from './kuzzle-provider';
 
+export * from './options';
 export default VueKuzzle;
 export { KuzzleProvider };
